695dcb6580d34bbee8ed803ec0818bb3abd2529e
Eric <erictomokigoto@gmail.com>
2020-05-11 19:40:20 -0700

    started working on a singly linked list

---
+++ Data Structures/SinglyLinkedList.java
@@ -0,0 +1,15 @@
+// implementation of a singly linked list and common operations
+// common operations: add to list, delete a node, check if certain data exists
+public class SinglyLinkedList {
+
+    private class Node {
+        Node next;
+        String data;
+    }
+
+    public void add(Node node) {
+
+    }
+
+
+}
