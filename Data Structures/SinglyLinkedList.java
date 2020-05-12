211a0b554fcf8685e391e077541042948375b1e0
Eric <erictomokigoto@gmail.com>
2020-05-12 09:58:12 -0700

    added a add method and a test client

--- Data Structures/SinglyLinkedList.java
+++ Data Structures/SinglyLinkedList.java
@@ -1,15 +1,34 @@
 // implementation of a singly linked list and common operations
-// common operations: add to list, delete a node, check if certain data exists
+// common operations: remove and return head node, delete a node, check if certain data exists, add to node
 public class SinglyLinkedList {
+    private Node head;
 
-    private class Node {
+    static class Node {
         Node next;
         String data;
+
+        public Node(String data) {
+            this.data = data;
+            next = null;
+        }
+
     }
 
-    public void add(Node node) {
-
+    public void add(String data) {
+        Node oldHead = head;
+        head = new Node("data");
+        head.next = oldHead;
     }
 
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.head = new Node("head");
+        list.add("second");
 
+
+        while (list.head != null) {
+            System.out.print(list.head.data + " ");
+            list.head = list.head.next;
+        }
+    }
 }
