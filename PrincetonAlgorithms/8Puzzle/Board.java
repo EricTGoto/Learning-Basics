bb6bd2803c832a61745cedf30092fbb1ad2c3fcc
Eric <erictomokigoto@gmail.com>
2020-05-30 16:26:43 -0700

    initial commit of skeleton functions, code will not compile

---
+++ PrincetonAlgorithms/8Puzzle/Board.java
@@ -0,0 +1,58 @@
+// data type that models an n by n board with sliding tiles
+// The 8 Puzzle is a common puzzle you can buy at a dollar store and you solve it by moving the numbered tiles
+// so that they end up in row major order
+// Written by Eric Goto, assignment from Princeton Coursera Algorithms course
+
+public class Board {
+
+    // initialize a board with n by n tiles
+    public Board(int[][] tiles) {
+
+    }
+
+    // returns a string representation of the board
+    public String toString() {
+
+    }
+
+    // returns the board dimensions
+    public int dimension() {
+
+    }
+
+    // returns the number of tiles out of place
+    public int hamming() {
+
+    }
+
+    // returns the sum of Manhattan distances between tiles and goal
+    public int manhattan() {
+
+    }
+
+    // checks if board is solved
+    public boolean isGoal() {
+
+    }
+
+    // checks if two boards are equal
+    public boolean equals(Object y) {
+
+    }
+
+    // returns the neighboring boards
+    public Iterable<Board> neighbors() {
+
+    }
+
+    // returns a board that is created by exchanging any pair of tiles
+    public Board twin() {
+
+    }
+
+    // for testing
+    public static void main(String[] args) {
+
+    }
+
+}
