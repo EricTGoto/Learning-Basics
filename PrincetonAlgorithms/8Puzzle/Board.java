b4c17983b0d9afaa45fe5c307df93e279d4ab5bf
Eric <erictomokigoto@gmail.com>
2020-05-31 11:35:59 -0700

    wrote toString and dimension methods

--- PrincetonAlgorithms/8Puzzle/Board.java
+++ PrincetonAlgorithms/8Puzzle/Board.java
@@ -22,47 +22,60 @@ public class Board {
 
     // returns a string representation of the board
     public String toString() {
+        StringBuilder boardString = new StringBuilder();
+        boardString.append(size + "\n ");
 
+        for (int k = 0; k < size; k++)
+            for (int j = 0; j < size; j++) {
+                boardString.append(board[j][k] + " ");
+                if (j == size - 1) boardString.append("\n ");
+            }
+        return boardString.toString();
     }
 
     // returns the board dimensions
     public int dimension() {
-
+        return size;
     }
 
     // returns the number of tiles out of place
     public int hamming() {
-
+        return 0;
     }
 
     // returns the sum of Manhattan distances between tiles and goal
     public int manhattan() {
-
+        return 0;
     }
 
     // checks if board is solved
     public boolean isGoal() {
-
+        return false;
     }
 
     // checks if two boards are equal
     public boolean equals(Object y) {
-
+        return false;
     }
 
     // returns the neighboring boards
     public Iterable<Board> neighbors() {
-
+        return null;
     }
 
     // returns a board that is created by exchanging any pair of tiles
     public Board twin() {
-
+        return null;
     }
 
     // for testing
     public static void main(String[] args) {
+        int[][] test = {{3, 4, 5},
+                {2, 1, 6},
+                {0, 9, 8}};
 
+        Board b = new Board(test);
+        System.out.print(b.toString());
     }
 
 }
