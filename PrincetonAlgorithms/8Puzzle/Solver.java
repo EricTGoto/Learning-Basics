dc0f0f2a7f8e926301e8b142133df0c0a967145b
Eric <erictomokigoto@gmail.com>
2020-06-03 11:55:38 -0700

    created the solver class which will solve the puzzle, wrote skeleton methods

---
+++ PrincetonAlgorithms/8Puzzle/Solver.java
@@ -0,0 +1,27 @@
+// This solver will employ the A* algorithm to solve the 8 puzzle
+public class Solver {
+
+    public Solver(Board initial) {
+
+    }
+
+    // returns true if the board is solvable
+    public boolean isSolvable() {
+        return false;
+    }
+
+    // returns the minimum number of boards to solve the board
+    public int moves() {
+        return 0;
+    }
+
+    // returns the sequence of moves if board is solvable, null if unsolvable
+    public Iterable<Board> solution() {
+        return null;
+    }
+
+    // testing
+    public static void main(String[] args) {
+
+    }
+}
